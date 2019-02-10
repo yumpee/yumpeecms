74b397cb39344673ff9daf7234cf7b99ef6deec1
Peter Odon <tinycall@yahoo.com>
2019-02-11 09:06:35 +1000

    Configuration files Update

---
+++ common/config/params-local.php
@@ -0,0 +1,7 @@
+<?php
+Yii::setAlias('@uploads', realpath(dirname(__FILE__).'/../../'));
+Yii::setAlias('@image_dir','http://localhost/myhouse/uploads/');
+Yii::setAlias('@themes','http://localhost/myhouse/frontend/themes/');
+
+return [
+];
